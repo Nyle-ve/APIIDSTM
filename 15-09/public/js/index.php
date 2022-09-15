bb385713951fec0a4738449972efb204149ac579
Nyle-ve <63523523+Nyle-ve@users.noreply.github.com>
2022-09-15 10:04:18 -0600

    Add files via upload

---
+++ 15-09/public/js/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acceso al panel</title>
+    <link rel="stylesheet" type="text/css" href="public/css/main.css"/>
+</head>
+<body>
+    <div>
+        <form action="">
+            <fieldset>
+                <legend>
+
+                </legend>
+                <label for="">Email</label>
+                <input type="text">
+                
+                <label for="">Password</label>
+                <input type="password">
+
+                <button>OK</button>
+            </fieldset>
+        </form>
+    </div>
+</body>
+</html>
