3934cbf3b86427e3d527e7eb4cb890e66ade3b39
Nyle-ve <63523523+Nyle-ve@users.noreply.github.com>
2022-09-27 14:09:34 -0600

    Add files via upload

---
+++ 27-09-02/public/js/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acceso al panel</title>
+    <!-- <link rel="stylesheet" type="text/css" href="public/css/main.css"> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css"></script>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container bg-dark text-white mt-3">
+    <form class="row align-items-start">
+    <div class="mb-3 col">
+        <label for="exampleInputEmail1" class="form-label">Email</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Ingrese su correo">
+    </div>
+    <div class="mb-3 col">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Ingrese la contraseña">
+    </div>
+    <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
