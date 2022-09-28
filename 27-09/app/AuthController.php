d693c03cfa0e39d6ba390efc4a3b718a0c12e555
Nyle-ve <63523523+Nyle-ve@users.noreply.github.com>
2022-09-27 21:39:15 -0600

    Update AuthController.php

--- 27-09/app/AuthController.php
+++ 27-09/app/AuthController.php
@@ -1,6 +1,16 @@
 <?php
-$authC = new AuthController();
+if(isset($_POST['action'])){
+    switch($_POST['action']){
+        case 'access':
+            $authC = new AuthController();
 
+            $email = strip_tags($_POST['email']);
+            $password = strip_tags($_POST['password']);
+            
+            $authC->login($email, $password);
+        break;
+    }
+}
 
 class AuthController
 {
@@ -8,7 +18,7 @@ class AuthController
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-        CURLOPT_URL => 'https://crud.jonathansoto.mx/api/login',
+        CURLOPT_URL => 'http://crud.jonathansoto.mx/api/login',
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => '',
         CURLOPT_MAXREDIRS => 10,
@@ -16,12 +26,24 @@ class AuthController
         CURLOPT_FOLLOWLOCATION => true,
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
         CURLOPT_CUSTOMREQUEST => 'POST',
-        CURLOPT_POSTFIELDS => array('email' => 'jsoto@uabcs.mx','password' => '4YakTNXPKEa7Wv'),
+        CURLOPT_POSTFIELDS => array('email' => $email,'password' => $password),
         ));
 
         $response = curl_exec($curl);
 
         curl_close($curl);
+        echo $response;
+
+        $response = json_decode($response);
+
+        if(isset($response->code) && $response->code > 0){
+            session_start();
+            $_SESSION['data'] = $response->data;
+            header("Location:../public/index.php");
+        }
+        else{
+            header("Location:../public/login.php?error=true");
+        }
         echo $response;
     }
 }
