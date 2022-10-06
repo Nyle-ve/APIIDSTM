e97d7bd21d2b44608f686bb502ec3be626c71d2d
Nyle-ve <63523523+Nyle-ve@users.noreply.github.com>
2022-10-06 09:43:20 -0600

    Add files via upload

---
+++ act-25/public/js/modalEditar.template.php
@@ -0,0 +1,50 @@
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Producto</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <form action="ProductController.php" method="POST">
+        <div class="mb-3">
+            <label for="exampleInput" class="form-label">Nombre</label>
+            <input type="text" class="form-control" name="name" id="name" aria-describedby="emailHelp">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInput" class="form-label">Categoría</label>
+            <input type="text" class="form-control" name="slug" id="slug">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInput" class="form-label">Descripción</label>
+            <input type="text" class="form-control" name="description" id="description">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInput" class="form-label">Características</label>
+            <input type="text" class="form-control" name="features" id="features">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInput" class="form-label">Marca</label>
+            <select id="brand_id" name="brand_id" class="form-select">
+            <?php foreach($marcas as $marca) { ?>
+                <option> <?php echo $marca->name ?> </option>
+            <?php } ?>
+            </select>
+        </div>
+        <div class="mb-3">
+            <label for="exampleInput" class="form-label">Características</label>
+            <input type="file" id="imagen" name="imagen" class="form-control" placeholder="imagen del producto" accept="image/*">
+        </div>
+        <input id="oculto_input" type="hidden" name="action" value="create">
+        <input id="id" type="hidden" name="id" value="create">
+        <input type="hidden" id="global_token" name="global_token" value="<?= $_SESSION['global_token'] ?>">
+      </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+        <button type="submit" class="btn btn-primary">Guardar</button>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ act-25/public/layout/head.template.php
@@ -0,0 +1,16 @@
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acceso al panel</title>
+    <!-- <link rel="stylesheet" type="text/css" href="public/css/main.css"> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css"></script>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+    <style>
+      aside{
+        height: 90vh;
+      }
+    </style>
